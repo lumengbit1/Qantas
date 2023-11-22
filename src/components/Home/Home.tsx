608781e9c25ad33d5f6a4f991be03b6fdf097055
Meng Lu <meng.lu@jetstar.com>
2023-11-22 20:32:11 +1100

    add slice example

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import type { RootState } from "../../store/store";
+import { decrement, increment } from "../../reducers/reducer";
 import { Root } from "./Home.style";
 
-const HomePage = () => <Root>Homepage</Root>;
+const HomePage = () => {
+  const count = useSelector((state: RootState) => state.reducer.value);
+  const dispatch = useDispatch();
+
+  return (
+    <Root>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </Root>
+  );
+};
 
 export default HomePage;
