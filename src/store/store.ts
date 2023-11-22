7c67ed9a9a6ad6745782409e6ef4526e9aaeda2d
Meng Lu <meng.lu@jetstar.com>
2023-11-22 20:11:51 +1100

    add reducer slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reducer from "../reducers/reducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { reducer: reducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
