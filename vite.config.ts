a1c81fb9282b9f96b595401b7409bc3139a4a7ab
Meng Lu <meng.lu@jetstar.com>
2023-11-22 19:31:12 +1100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
+  build: {
+    outDir: "build",
+  },
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    css: {
+      modules: {
+        classNameStrategy: "non-scoped",
+      },
+    },
+    coverage: {
+      reporter: ["text", "html"],
+      exclude: ["node_modules/", "src/setupTests.ts"],
+    },
+  },
+}));
