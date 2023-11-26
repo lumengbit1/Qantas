c5f0939ce2168705210bc247c285fc189d42c044
Meng Lu <meng.lu@jetstar.com>
2023-11-26 11:37:44 +1100

    test cases

--- src/reducers/reducer.test.ts
+++ src/reducers/reducer.test.ts
@@ -1,8 +1,23 @@
-import reducer from "./reducer";
+import reducer, { sorting } from "./reducer";
+import { SortMapping } from "./constants";
 import mockData from "../assets/mockData/mock_hotel_data_with_picsum.json";
+import expectedData_Ascending from "./mockData/expectedData_Ascending.json";
+import expectedData_Descending from "./mockData/expectedData_Descending.json";
 
 describe("Reducer", () => {
   it("should return the initial state", () => {
     expect(reducer(undefined, { type: undefined })).toEqual(mockData);
+  });
+
+  it("should return ascending data", () => {
+    expect(reducer(mockData, sorting(SortMapping.Ascending))).toEqual(
+      expectedData_Ascending
+    );
+  });
+
+  it("should return descending data", () => {
+    expect(reducer(mockData, sorting(SortMapping.Descending))).toEqual(
+      expectedData_Descending
+    );
   });
 });
