e2abb67e6a7f9bb919556c28f982c83a40f2c328
Meng Lu <meng.lu@jetstar.com>
2023-11-26 14:05:16 +1100

    test cases

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { createMemoryHistory, History } from "history";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import Pagination from "./Pagination";
 import { Provider } from "react-redux";
 import { store } from "../../store/store";
+
+let mockSearchParam = "";
+
+vi.mock("react-router-dom", async () => {
+  const rest = await vi.importActual("react-router-dom");
+
+  return Object.assign({}, rest, {
+    useSearchParams: () => {
+      const [params, setParams] = useState(
+        new URLSearchParams(mockSearchParam)
+      );
+      return [
+        params,
+        (newParams: string) => {
+          mockSearchParam = newParams;
+          setParams(new URLSearchParams(newParams));
+        },
+      ];
+    },
+  });
+});
 
 describe("Pagination Component", () => {
   let history: History;
@@ -44,6 +66,7 @@ describe("Pagination Component", () => {
     expect(paginationButton).toHaveStyle({ "pointer-events": "none" });
     expect(paginationPrev).not.toHaveAttribute("disabled");
     expect(paginationNext).not.toHaveAttribute("disabled");
+    expect(mockSearchParam).toContain({ page: "2" });
   });
 
   it("should disabled in the pagination_3 button, and next button is disabled", () => {
@@ -54,17 +77,23 @@ describe("Pagination Component", () => {
     expect(paginationButton).toHaveStyle({ "pointer-events": "none" });
     expect(paginationPrev).not.toHaveAttribute("disabled");
     expect(paginationNext).toHaveAttribute("disabled");
+    expect(mockSearchParam).toContain({ page: "3" });
   });
 
   it("should switch to pagination_2 and panigination_3 when clicking on next", () => {
+    const paginationButton1 = screen.getByTestId("pagination_1");
     const paginationButton2 = screen.getByTestId("pagination_2");
     const paginationPrev = screen.getByTestId("pagination_prev");
     const paginationNext = screen.getByTestId("pagination_next");
+
+    fireEvent.click(paginationButton1);
+    expect(mockSearchParam).toContain({ page: "1" });
 
     fireEvent.click(paginationNext);
     expect(paginationButton2).toHaveStyle({ "pointer-events": "none" });
     expect(paginationPrev).not.toHaveAttribute("disabled");
     expect(paginationNext).not.toHaveAttribute("disabled");
+    expect(mockSearchParam).toContain({ page: "2" });
 
     const paginationButton3 = screen.getByTestId("pagination_3");
     fireEvent.click(paginationNext);
@@ -72,6 +101,7 @@ describe("Pagination Component", () => {
     expect(paginationButton2).toHaveStyle({ "pointer-events": "auto" });
     expect(paginationPrev).not.toHaveAttribute("disabled");
     expect(paginationNext).toHaveAttribute("disabled");
+    expect(mockSearchParam).toContain({ page: "3" });
   });
 
   it("should switch to pagination_2 and panigination_1 when clicking on prev", () => {
@@ -80,12 +110,14 @@ describe("Pagination Component", () => {
     const paginationNext = screen.getByTestId("pagination_next");
 
     fireEvent.click(paginationButton3);
+    expect(mockSearchParam).toContain({ page: "3" });
     fireEvent.click(paginationPrev);
     const paginationButton2 = screen.getByTestId("pagination_2");
     expect(paginationButton2).toHaveStyle({ "pointer-events": "none" });
     expect(paginationButton3).toHaveStyle({ "pointer-events": "auto" });
     expect(paginationPrev).not.toHaveAttribute("disabled");
     expect(paginationNext).not.toHaveAttribute("disabled");
+    expect(mockSearchParam).toContain({ page: "2" });
 
     const paginationButton1 = screen.getByTestId("pagination_1");
     fireEvent.click(paginationPrev);
@@ -93,5 +125,6 @@ describe("Pagination Component", () => {
     expect(paginationButton2).toHaveStyle({ "pointer-events": "auto" });
     expect(paginationPrev).toHaveAttribute("disabled");
     expect(paginationNext).not.toHaveAttribute("disabled");
+    expect(mockSearchParam).toContain({ page: "1" });
   });
 });
